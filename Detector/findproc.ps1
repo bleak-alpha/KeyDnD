b3c41268aa3563391922cff3054c858cbca23218
bleak-alpha <bleak-alpha@protonmail.ch>
2023-04-23 17:38:40 +0530

    including past code

--- Detector/findproc.ps1
+++ Detector/findproc.ps1
@@ -5,22 +5,68 @@ if ($process) {
     Write-Host "`0"
     }#>
 
-. "isoproc.ps1"
+
+<#. "isoproc.ps1"
+
+$proc = "notepad.exe"
 
 try {
-    # code that might throw an exception
-    $file = Get-Item "C:\NonexistentFile.txt"
-    Write-Output "File found: $($file.FullName)"
-} catch {
-    # handle the exception
-    Write-Error "An error occurred: $($_.Exception.Message)"
-} finally {
-    # code that runs whether or not an exception was thrown
-    Write-Output "Cleanup code"
+    # Get all running processes
+    $processes = Get-Process
+
+    # Search for the specified process name
+    for ($i = 0; $i -lt $processes.Count; $i++) {
+        if ($processes[$i].Name -eq $proc) {
+            # Output details for the found process
+            Write-Output "Process name: $($processes[$i].Name)"
+            Write-Output "Process ID: $($processes[$i].Id)"
+            Write-Output "Start time: $($processes[$i].StartTime)"
+            Write-Output "Path: $($processes[$i].Path)"
+            Write-Output "Memory usage: $($processes[$i].WorkingSet64) bytes"
+            break
+        }
+    }
+
+    if ($i -eq $processes.Count) {
+        Write-Output "$proc is not running"
+    }
 }
+catch {
+    Write-Error "Error occurred: $($_.Exception.Message)"
+}
+#>
+
+<#-------------------------------------------------------------#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [ScriptBlock]$ProcessList
+)
+
+$process_name = "notepad.exe"
+
 try {
-    $process = Get-Process -Name "keylogger"
-} catch {
-    # handle the exception
-    Write-Error "An error occurred: $($_.Exception.Message)"
+    # Call the script block to retrieve the process list
+    $processes = Invoke-Command -ScriptBlock $ProcessList
+
+    # Search for the specified process name
+    for ($i = 0; $i -lt $processes.Count; $i++) {
+        if ($processes[$i].Name -eq $process_name) {
+            # Output details for the found process
+            Write-Output "Process name: $($processes[$i].Name)"
+            Write-Output "Process ID: $($processes[$i].Id)"
+            Write-Output "Start time: $($processes[$i].StartTime)"
+            Write-Output "Path: $($processes[$i].Path)"
+            Write-Output "Memory usage: $($processes[$i].WorkingSet64) bytes"
+            break
+        }
+    }
+
+    if ($i -eq $processes.Count) {
+        Write-Output "$process_name is not running"
+    }
+}
+catch {
+    Write-Error "Error occurred: $($_.Exception.Message)"
 }
--- Detector/isoproc.ps1
+++ Detector/isoproc.ps1
@@ -1,4 +1,4 @@
-function get-new-process-names {
+<#function get-new-process-names {
     $processes = Get-Process
     $new_processes = @()
 
@@ -9,4 +9,35 @@ function get-new-process-names {
     }
 
     return $new_processes
+}#>
+
+<#
+# Set the time range for process start time
+$start_time = (Get-Date).AddMinutes(-10)
+
+# Get all running processes
+$processes = Get-Process
+
+# Filter out system processes and processes that started before $start_time
+$new_processes = $processes | Where-Object {
+    $_.StartTime -gt $start_time -and $_.Name -notmatch '^System|^Idle$'
 }
+
+# Store the process names in an array
+$process_names = $new_processes.Name
+
+# Output the process names
+#Write-Output "New processes in the last 10 minutes: $($process_names -join ', ')"
+#>
+<#-------------------------------------------------------------------------------------------------------#>
+# Get the current time
+$now = Get-Date
+
+# Get the process list and filter out system processes
+$processes = Get-Process | Where-Object { $_.Name -notin @("Idle", "System") }
+
+# Filter the process list by start time (within the last 10 minutes)
+$ProcessList = $processes | Where-Object { $now.Subtract($_.StartTime).TotalMinutes -lt 10 }
+
+# Pass the recent process list to script2.ps1
+.\findproc.ps1 -ProcessList $ProcessList
