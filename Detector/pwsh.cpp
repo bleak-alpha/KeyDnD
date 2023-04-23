bc4f65149bfeb7de57aea3709e9af9f947646094
bleak-alpha <bleak-alpha@protonmail.ch>
2023-04-23 16:02:05 +0530

    cleaning && final touchups

---
+++ Detector/pwsh.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+
+//global
+using namespace std;
+int output = 1;
+
+
+void isoproc() {
+    system("isoproc.ps1")
+}
+
+void findproc() {
+    
+}
+
+int main() {
+    int output = 1;
+
+
+    return 0;
+}
